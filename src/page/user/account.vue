3d39201fbf0606658570c43588549b81431f6ea0
ranqiyang11 <ranqiyang@jiayunhui.com>
2018-12-12 13:19:44 +0800

    个人中心 账户安全 上传身份证

--- src/page/user/account.vue
+++ src/page/user/account.vue
@@ -14,12 +14,16 @@ dheader
         a(@click="updatePasswd") 更新密码
     .clear
     .mt-50.title 应国家全网实名制的要求，请认证个人身份信息
-    input.mt-40(v-model="realname", placeholder="请输入真实姓名进行认证")
+    //- input.mt-40(v-model="realname", placeholder="请输入真实姓名进行认证")
     .wrap.mt-40
-      input.fl.again(v-model="realid", placeholder="请输入身份号码进行认证")
-      .btn.fl.ml-20.authoriedID
-        a(@click="authoriedID") 认证
-
+      input.fl(v-model="license_id", placeholder="请输入身份号码进行认证")
+      .clear
+    .userImg.mt-4
+      p 请上传本人手持身份证照片
+      img(:src="userImg", @click="uploadWork")
+      upload(type="work", maxsize="2*1024", class="upload")
+    .btn.authoriedID
+      a(@click="authoriedID") 认证
 </template>
 <script>
 import {Vue, dheader} from 'src/assets/js/page';
@@ -28,6 +32,7 @@ import cutimg from 'src/public/cutimg/cutimg';
 import utils from 'src/assets/js/utils';
 import api from 'src/assets/js/api';
 import $ from 'jquery';
+import upload from 'src/public/upload/upload';
 
 function without(array, val) {
     let len = array.length;
@@ -44,17 +49,33 @@ export default {
     components: {
         dheader,
         cutimg,
-        tab
+        tab,
+        upload
     },
     asyncData (resolve, reject) {
         // 绑定用户信息
         window.userStat.done(function () {
-            resolve(this.data);
+          this.data.userImg = this.data.license_pic ? this.data.license_pic : './userBg.png';
+          resolve(this.data);
         });
+    },
+    events: {
+      // 上传类似作品
+      uploadComplete(ret) {
+        this.userImg = ret.data[0][1]
+      }
     },
     methods: {
         authoriedID() {
-
+            let data = {
+              license_pic: this.userImg,
+              license_id: this.license_id
+            }
+            const url = '/api/users/' + this.id + '/';
+            api.patch({
+                url,
+                data
+            });
         },
         updatePasswd() {
             let url = constant.API.USER_RESETPASSWORD;
@@ -67,7 +88,10 @@ export default {
                 url,
                 data
             });
-        }
+        },
+        uploadWork() {
+          $('#upload').click();
+        },
     },
     data () {
         let id = utils.getURLParam('id');
@@ -111,6 +135,7 @@ export default {
     border-radius: 10px;
     margin-right: 50px;
     height: 920px;
+    overflow-y: auto;
     .avatar-container,
     .avatar,
     .avatar-container {
@@ -150,6 +175,27 @@ export default {
       width: 100%;
       &.again {
         width: 80%;
+      }
+    }
+    .authoriedID{
+      width: 84px;
+      margin: 23px auto;
+    }
+    .userImg{
+      p{
+        text-align: center;
+        font-size: 14px;
+        margin-bottom: 8px;
+      }
+      img{
+        width: 283px;
+        height: 195px;
+        margin: 0 auto;
+        display: block;
+        cursor: pointer;
+      }
+      .upload{
+        display: none;
       }
     }
   }

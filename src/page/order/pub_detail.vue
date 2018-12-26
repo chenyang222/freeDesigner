8b3180c9c45b427d5bff51573c16f7e444af30fd
王建鹏 <amyas.wang@qingclass.com>
2018-12-26 16:13:25 +0800

    fix

--- src/page/order/pub_detail.vue
+++ src/page/order/pub_detail.vue
@@ -295,7 +295,7 @@ export default {
                 message:evaluateVal
               }
             }).done(function(){
-              window.locaiton.reload()
+              window.location.reload()
             })
           })
         },
